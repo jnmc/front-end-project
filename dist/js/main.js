fa687b423e0ee3a3d2436fc1161b7d041666a50e
jnmc <jiantan29@gmail.com>
2014-09-22 07:21:59 +1000

    front end project

---
+++ dist/js/main.js
@@ -0,0 +1,43 @@
+/* ==========================================================================
+
+    Project: my project
+    Author: 
+    Last updated: Mon Sep 22 2014 06:54:38
+
+   ========================================================================== */
+
+(function($) {
+
+  'use strict';
+
+  var App = {
+
+    /**
+     * Init Function
+     */
+    init: function() {
+      // App.feature1();
+      // App.feature2();
+    },
+
+    /**
+     * Custom feature 1
+     */
+    feature1: function() {
+
+    },
+
+    /**
+     * Custom feature 2
+     */
+    feature2: function() {
+
+    }
+
+  };
+
+  $(function() {
+    App.init();
+  });
+
+})(jQuery);
